59386a7785ec95a4d6dcfa4816586a688391fda9
Sergei Vjuev <vjuev2013@yandex.ru>
2020-05-06 21:39:59 +0300

    seventh commit

--- controllers/users.js
+++ controllers/users.js
@@ -1,8 +1,11 @@
+/* eslint-disable no-shadow */
+/* eslint-disable consistent-return */
 /* eslint-disable no-throw-literal */
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const users = require('../models/users');
 
+const { JWT_SECRET = '402271a490b1da84693aaed3aa8b3dbdade403a609d072c6c10b5e4b55d53880' } = process.env;
 module.exports.getusers = (req, res) => {
   users.find({})
     .then((user) => res.send({ data: user }))
@@ -12,10 +15,12 @@ module.exports.postusers = (req, res) => {
   const {
     name, about, avatar, email, password,
   } = req.body;
+  if (password.length < 8) {
+    return res.status(401).send({ message: 'Длина пароля долна быть больше 8 символов' });
+  }
   bcrypt.hash(password, 10)
-  // eslint-disable-next-line no-shadow
-    .then((password) => users.create({
-      name, about, avatar, email, password,
+    .then((hash) => users.create({
+      name, about, avatar, email, password: hash,
     }))
     .then((card) => res.send({ data: card }))
     .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
@@ -35,7 +40,7 @@ module.exports.login = (req, res) => {
 
   return users.findUserByCredentials(email, password)
     .then((user) => {
-      const token = jwt.sign({ _id: user._id }, '402271a490b1da84693aaed3aa8b3dbdade403a609d072c6c10b5e4b55d53880', { expiresIn: '7d' });
+      const token = jwt.sign({ _id: user._id }, JWT_SECRET, { expiresIn: '7d' });
       res.cookie('name', token, { maxAge: 3600000 * 24 * 7 });
       res.send({ token });
     })
